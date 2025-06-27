e2ff908f2554c89cc4c34f78e63f653c617f487d
cthies <caroline.thies@capgemini.com>
2025-06-27 12:22:05 +0200

    check if ensure-longpaths removes idea.properties file

--- cli/src/test/integration-tests/z_ensure-longpaths.sh
+++ cli/src/test/integration-tests/z_ensure-longpaths.sh
@@ -2,7 +2,9 @@ if doIsWindows
 then
   echo "Running ensure git config longpaths gets set integration test"
   touch "$HOME"/.gitconfig
+  assertThat "${IDE_ROOT}/${TEST_PROJECT_NAME}/workspaces/main/idea.properties" exists
   $IDE -f install
+  assertThat "${IDE_ROOT}/${TEST_PROJECT_NAME}/workspaces/main/idea.properties" exists
   gitconfig_path="$HOME"/.gitconfig
   fileContent=$(cat "$gitconfig_path")
   assertThat "$fileContent" contains "longpaths"
