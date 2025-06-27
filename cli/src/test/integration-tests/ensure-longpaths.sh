3e483e7a9f9835616c7b3b346fc3a9e9b52b1351
cthies <caroline.thies@capgemini.com>
2025-06-27 13:10:34 +0200

    check if installation in git longpaths test is necessary

--- cli/src/test/integration-tests/ensure-longpaths.sh
+++ cli/src/test/integration-tests/ensure-longpaths.sh
@@ -4,8 +4,8 @@ then
   pwd
   echo "Running ensure git config longpaths gets set integration test"
   touch "$HOME"/.gitconfig
-  $IDE -f install
- # gitconfig_path="$HOME"/.gitconfig
-  #fileContent=$(cat "$gitconfig_path")
-#  assertThat "$fileContent" contains "longpaths"
+  #$IDE -f install
+  gitconfig_path="$HOME"/.gitconfig
+  fileContent=$(cat "$gitconfig_path")
+  assertThat "$fileContent" contains "longpaths"
 fi
